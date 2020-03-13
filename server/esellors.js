5386e3f5a13fd593e5f26fc55ce09edb6f0cc511
Eric Sellors <github@esellors.com>
2020-03-12 21:29:50 -0500

    server updated

--- server/esellors.js
+++ server/esellors.js
@@ -4,4 +4,6 @@ const app = express();
 
 const { SERVER_PORT } = process.env;
 
+app.use( express.static( `${__dirname}/../build` ));
+
 app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`));
