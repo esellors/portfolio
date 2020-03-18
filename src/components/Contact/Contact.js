7c55e867e5e7cb57705e4ede6a17813494afc761
Eric Sellors <github@esellors.com>
2020-03-17 22:39:58 -0500

    decreased font sizes even further

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
         <section id='contact' className='contact_section sections'>
             <h2 className='section_titles'>contact</h2>
             <p>I'd love to hear from you!</p>
-            <p>Message on <a href='https://www.linkedin.com/in/esellors/' target='_blank' rel='noopener noreferrer'>LinkedIn</a></p>
+    <p>Message on <a href='https://www.linkedin.com/in/esellors/' target='_blank' rel='noopener noreferrer'>LinkedIn</a></p>
         </section>
     );
 }
