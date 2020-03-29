7adfbde411724a0e365c98dde3d8e0d0d1f0b3d2
Eric Sellors <github@esellors.com>
2020-03-29 17:07:59 -0500

    added email to contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,12 @@ const Contact = () => {
         <section id='contact' className='contact_section sections'>
             <h2 className='section_titles'>contact</h2>
             <p>I'd love to hear from you!</p>
-    <p>Message on <a href='https://www.linkedin.com/in/esellors/' target='_blank' rel='noopener noreferrer'>LinkedIn</a></p>
+            <p>
+                <a href='https://www.linkedin.com/in/esellors/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+            </p>
+            <p>
+                <a href='mailto:ericdev@esellors.com'>ericdev&#64;esellors.com</a>
+            </p>
         </section>
     );
 }
